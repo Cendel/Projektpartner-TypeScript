70478110f6ec11b705019d7f4ce521e36b79192d
Cendel Altunisik <cendelaltinisik@gmail.com>
2024-11-11 19:31:05 +0100

    clears excess comments

--- src/api/contact-service.ts
+++ src/api/contact-service.ts
@@ -1,10 +1,8 @@
 import axios, { AxiosResponse } from "axios";
 import authHeader from "../helpers/functions/auth-header";
 
-// API URL'i environment variable'dan alıyoruz
 const API_URL = process.env.REACT_APP_BACKEND_URL;
 
-// Message türünü tanımlıyoruz
 interface Message {
   created_date: string;
   sender: number;
@@ -21,21 +19,18 @@ export const sendMessage = (message: Message): Promise<AxiosResponse> => {
 
 // ADMIN ENDPOINTS
 
-// Message listesi dönecek
 export const listMessages = (): Promise<AxiosResponse<Message[]>> => {
   return axios.get(`${API_URL}/messages/`, {
     headers: authHeader(),
   });
 };
 
-// Tek bir message dönecek
 export const getMessage = (id: number): Promise<AxiosResponse<Message>> => {
   return axios.get(`${API_URL}/messages/${id}/`, {
     headers: authHeader(),
   });
 };
 
-// Message silme
 export const deleteMessage = (id: number): Promise<AxiosResponse> => {
   return axios.delete(`${API_URL}/messages/${id}/`, {
     headers: authHeader(),
