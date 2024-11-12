eeae8092e66ebc4882ec17bd58c3f971bdf97936
Cendel Altunisik <cendelaltinisik@gmail.com>
2024-11-12 09:56:48 +0100

    transforms user-service into TS

---
+++ src/api/user-service.ts
@@ -0,0 +1,53 @@
+import apiRequest from "./apiRequest";
+
+interface User {
+  name?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+  job?: string;
+  location?: string;
+  about?: string;
+  phone?: string;
+  website?: string;
+}
+
+// USER ENDPOINTS
+
+export const register = (user: User) => {
+  return apiRequest<User>("post", "/register/", user);
+};
+
+export const login = (user: User) => {
+  return apiRequest<User>("post", "/login/", user);
+};
+
+export const getUser = () => {
+  return apiRequest<User>("get", "/user/");
+};
+
+export const getUserById = (id: number) => {
+  return apiRequest<User>("get", `/user/${id}/`);
+};
+
+export const updateUser = (user: User) => {
+  return apiRequest<User>("put", "/user/", user);
+};
+
+// ADMIN ENDPOINTS
+
+export const getUsersAdmin = () => {
+  return apiRequest<User>("get", "/auth/users/");
+};
+
+export const getUserAdmin = (id: number) => {
+  return apiRequest<User>("get", `/user/${id}/`);
+};
+
+export const updateUserAdmin = (id: number, user: User) => {
+  return apiRequest<User>("put", `/user/${id}/update/`, user);
+};
+
+export const deleteUserAdmin = (id: number) => {
+  return apiRequest<User>("delete", `/user/${id}/delete/`);
+};
