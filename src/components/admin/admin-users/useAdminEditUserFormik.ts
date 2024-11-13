4cea1ad5f73e3134fa07d9949c63cfb4b12b20b3
Cendel Altunisik <cendelaltinisik@gmail.com>
2024-11-13 04:16:07 +0100

    creates ApiError type in types folder and transforms useAdminEditUserFormik into TS

--- src/components/admin/admin-users/useAdminEditUserFormik.ts
+++ src/components/admin/admin-users/useAdminEditUserFormik.ts
@@ -3,11 +3,14 @@ import { useState } from "react";
 import { updateUserAdmin } from "../../../api/user-service";
 import { adminEditUserValidationSchema } from "../../../helpers/validationSchemas";
 import { toast } from "../../../helpers/functions/swal";
+import User from "../../../entities/User";
+import { ApiError } from "../../../types/ApiError";
 
-const useAdminEditUserFormik = (props) => {
+const useAdminEditUserFormik = (props: User) => {
   const [updating, setUpdating] = useState(false);
 
   const initialValues = {
+    id: props.id,
     name: props.name,
     email: props.email,
     job: props.job,
@@ -17,13 +20,18 @@ const useAdminEditUserFormik = (props) => {
     about: props.about,
   };
 
-  const onSubmit = async (values) => {
+  const onSubmit = async (values: User) => {
     setUpdating(true);
     try {
       await updateUserAdmin(props.id, values);
       toast("Das Profil wurde erfolgreich aktualisiert.", "success");
-    } catch (err) {
-      toast(err.response.data.message, "error");
+    } catch (err: unknown) {
+      const error = err as ApiError;
+      if (error.response?.data?.message) {
+        toast(error.response.data.message, "error");
+      } else {
+        toast("An unexpected error occurred.", "error");
+      }
     } finally {
       setUpdating(false);
     }
