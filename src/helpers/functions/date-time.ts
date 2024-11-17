fd7a96687b1d52c60d3b7f785a03d5c770b274f8
Cendel Altunisik <cendelaltinisik@gmail.com>
2024-11-17 19:03:58 +0100

    converts date-time.js into TS

--- src/helpers/functions/date-time.ts
+++ src/helpers/functions/date-time.ts
@@ -5,10 +5,10 @@ export const getCurrentDate = () => {
   return moment().format("YYYY-MM-DD");
 };
 
-export const formatDateToYYYYMMDD = (date) => {
+export const formatDateToYYYYMMDD = (date: string) => {
   return moment(date).format("YYYY-MM-DD");
 };
 
-export const convertCurrentDateToUserFormat = (date) => {
+export const convertCurrentDateToUserFormat = (date: string) => {
   return moment(date).format("DD MMMM YYYY");
 };
