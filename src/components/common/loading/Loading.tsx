f3e4ad7ea8ba22a3ba922de2506b27499c8a150f
Cendel Altunisik <cendelaltinisik@gmail.com>
2024-11-19 02:49:04 +0100

    Convert Loading.js into TS

---
+++ src/components/common/loading/Loading.tsx
@@ -0,0 +1,12 @@
+import { Spinner } from "react-bootstrap";
+import "./loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <Spinner animation="border" className="ada" />
+    </div>
+  );
+};
+
+export default Loading;
